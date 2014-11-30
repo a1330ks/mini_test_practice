2d24f81004fac77589cb1357d7f04c912a5e4b4c
Kei SAKAMOTO <a1330ks@aiit.ac.jp>
2014-11-30 01:10:39 +0000

    first commit

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -1,5 +1,42 @@
 require "mini_test_practice/version"
 
 module MiniTestPractice
-  # Your code goes here...
+  class MyClass
+    def odd?(value)
+      (value % 2) == 1
+    end
+
+    def check_number?(value)
+      value >= 1000 && value <= 9999
+    end
+
+    def enough_length?(str)
+      len = str.split(//).size
+      len >= 3 && len <= 8
+    end
+
+    def divide(num1,num2)
+      val = num2.modulo(num1)
+      return val
+    end
+
+    def fizz_buzz(num)
+      isFizz = num.modulo(3) == 0
+      isBuzz = num.modulo(5) == 0
+      if isFizz && isBuzz then
+        return "FizzBuzz"
+      elsif isFizz
+        return "Fizz"
+      elsif isBuzz
+        return "Buzz"
+      else
+        return ""
+      end
+    end
+
+    def say_hello()
+      print "Hello"
+    end
+
+  end
 end
--- test/test_mini_test_practice.rb
+++ test/test_mini_test_practice.rb
@@ -1,11 +1,54 @@
 require 'minitest_helper'
 
 class TestMiniTestPractice < MiniTest::Unit::TestCase
+  def setup
+    @my_class = ::MiniTestPractice::MyClass.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::MiniTestPractice::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_odd?
+    assert_equal false, @my_class.odd?(0)
+    assert_equal true, @my_class.odd?(1)
+    assert_equal false, @my_class.odd?(2) 
   end
+
+  def test_check_number?
+    assert_equal false, @my_class.check_number?(999)
+    assert_equal true, @my_class.check_number?(1000)
+    assert_equal true, @my_class.check_number?(9999)
+    assert_equal false, @my_class.check_number?(10000)
+  end
+
+  def test_enough_length?
+    assert_equal false, @my_class.enough_length?("ab")
+    assert_equal true, @my_class.enough_length?("abc")
+    assert_equal true, @my_class.enough_length?("abcdefgh")
+    assert_equal false, @my_class.enough_length?("abcdefghi")
+  end
+
+  def test_divide
+    assert_equal 0, @my_class.divide(1,3)
+    assert_equal 1, @my_class.divide(2,3)
+    assert_equal 0, @my_class.divide(3,3)
+    assert_equal 3, @my_class.divide(4,3)
+    assert_raises (ZeroDivisionError) {@my_class.divide(0,3)}
+  end
+
+  def test_fizz_buzz
+    assert_equal "", @my_class.fizz_buzz(1)
+    assert_equal "Fizz", @my_class.fizz_buzz(3)
+    assert_equal "Buzz", @my_class.fizz_buzz(5)
+    assert_equal "FizzBuzz", @my_class.fizz_buzz(15)
+  end
+
+  def test_say_hello
+    out, err = capture_io do
+      @my_class.say_hello
+    end
+    assert_equal "Hello", out  
+  end
+
 end
